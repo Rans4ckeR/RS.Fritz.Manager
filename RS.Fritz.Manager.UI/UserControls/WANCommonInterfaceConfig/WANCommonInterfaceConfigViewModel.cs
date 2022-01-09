0645ec4e74b67d7f32634debffe39f9ce56c9fc5
RoSchmi <Dr-Roland-Schmidt@t-online.de>
2022-01-09 11:37:48 +0100

    TotalBytesReceived now works completely

--- RS.Fritz.Manager.UI/UserControls/WANCommonInterfaceConfig/WANCommonInterfaceConfigViewModel.cs
+++ RS.Fritz.Manager.UI/UserControls/WANCommonInterfaceConfig/WANCommonInterfaceConfigViewModel.cs
@@ -1,4 +1,15 @@
-﻿namespace RS.Fritz.Manager.UI
+﻿//
+// BasicHttpBinding Klasse
+// https://docs.microsoft.com/de-de/dotnet/api/system.servicemodel.basichttpbinding?view=dotnet-plat-ext-6.0
+//
+// Introduction to the MVVM Toolkit
+// https://docs.microsoft.com/en-us/windows/communitytoolkit/mvvm/introduction
+/// <summary>
+/// /
+/// </summary>
+
+
+namespace RS.Fritz.Manager.UI
 {
     using System;
     using System.Threading.Tasks;
@@ -9,74 +20,20 @@
 
     internal sealed class WANCommonInterfaceConfigViewModel : FritzServiceViewModel
     {
-        // private readonly DispatcherTimer autoRefreshTimer;
-
         private bool autoRefresh;
 
         private WanCommonInterfaceConfigGetTotalBytesReceivedResponse? wanCommonInterfaceConfigGetTotalBytesReceivedResponse;
-
-        //private WANCommonInterfaceConfigGetDSLDiagnoseInfoResponse? wanDslInterfaceConfigGetDSLDiagnoseInfoResponse;
-        //private WANCommonInterfaceConfigGetDSLInfoResponse? wanDslInterfaceConfigGetDSLInfoResponse;
-        //private WANCommonInterfaceConfigGetStatisticsTotalResponse? wanDslInterfaceConfigGetStatisticsTotalResponse;
-
 
         // Constructor
         public WANCommonInterfaceConfigViewModel(DeviceLoginInfo deviceLoginInfo, ILogger logger, IFritzServiceOperationHandler fritzServiceOperationHandler)
             : base(deviceLoginInfo, logger, fritzServiceOperationHandler)
         {
-
             int dummy2 = 1;
-            // WANCommonInterfaceConfigViewModel
-
-            //WANCommonInterfaceConfigViewModel = WANCommonInterfaceConfigViewModel();
-
-            //WANCommonInterfaceConfigInfoControlViewModel = new WANCommonInterfaceConfigInfoControlViewModel();
-            /*
-            autoRefreshTimer = new DispatcherTimer
-            {
-                Interval = TimeSpan.FromSeconds(3d)
-            };
-            */
+            
         }
 
-
-
-        /*
-        public bool AutoRefresh
-        {
-            get => autoRefresh;
-            set
-            {
-                if (SetProperty(ref autoRefresh, value))
-                {
-                    if (value)
-                        autoRefreshTimer.Start();
-                    else
-                        autoRefreshTimer.Stop();
-                }
-            }
-        }
         
-        */
 
-        /*
-        public WANCommonInterfaceConfigGetDSLDiagnoseInfoResponse? WANCommonInterfaceConfigGetDSLDiagnoseInfoResponse
-        {
-            get => wanCommonInterfaceConfigGetDSLDiagnoseInfoResponse; set { _ = SetProperty(ref wanCommonInterfaceConfigGetDSLDiagnoseInfoResponse, value); }
-        }
-
-        public WANCommonInterfaceConfigGetDSLInfoResponse? WANCommonInterfaceConfigGetDSLInfoResponse
-        {
-            get => wanCommonInterfaceConfigGetDSLInfoResponse; set { _ = SetProperty(ref wanCommonInterfaceConfigGetDSLInfoResponse, value); }
-        }
-
-        public WANCommonInterfaceConfigInfoControlViewModel WANCommonInterfaceConfigInfoControlViewModel { get; set; }
-
-        public WANCommonInterfaceConfigGetStatisticsTotalResponse? WANCommonInterfaceConfigGetStatisticsTotalResponse
-        {
-            get => wanCommonInterfaceConfigGetStatisticsTotalResponse; set { _ = SetProperty(ref wanCommonInterfaceConfigGetStatisticsTotalResponse, value); }
-        }
-        */
 
 
         public WanCommonInterfaceConfigGetTotalBytesReceivedResponse? WanCommonInterfaceConfigGetTotalBytesReceivedResponse
@@ -84,7 +41,7 @@
             get => wanCommonInterfaceConfigGetTotalBytesReceivedResponse; set { _ = SetProperty(ref wanCommonInterfaceConfigGetTotalBytesReceivedResponse, value); }
         }
 
-
+        /*
         public override void Receive(PropertyChangedMessage<bool> message)
         {
             base.Receive(message);
@@ -104,76 +61,29 @@
                     }
             }
         }
-
-
-        // RoSchmi
-        /*
-        protected override async Task DoExecuteDefaultCommandAsync()
-        {
-            await Domain.TaskExtensions.WhenAllSafe(new[]
-                {
-                    GetWanDslInterfaceConfigGetDSLDiagnoseInfoAsync(),
-                    GetWanDslInterfaceConfigGetDSLInfoAsync(),
-                    GetWanDslInterfaceConfigGetInfoAsync(),
-                    GetWanDslInterfaceConfigGetStatisticsTotalAsync()
-                });
-        }
         */
 
-        //protected override async Task DoExecuteDefaultCommandAsync()
-
-        //public DeviceInfoSetProvisioningCodeViewModel DeviceInfoSetProvisioningCodeViewModel { get; }
-
-        //public WANCommonInterfaceConfigViewModel WANCommonInterfaceConfigViewModel { get; }
-
-        //public WanCommonInterfaceConfigGetTotalBytesReceivedResponse wanCommonInterfaceConfigGetTotalBytesReceivedResponse { get; }
-
-
-
-
-
         protected override async Task DoExecuteDefaultCommandAsync()
         {
-            
             await Domain.TaskExtensions.WhenAllSafe(new[]
                 {
                    GetWanCommonInterfaceConfigGetTotalReceivedBytesAsync(),
                 });
-            
         }
 
         private async Task GetWanCommonInterfaceConfigGetTotalReceivedBytesAsync()
         {
             wanCommonInterfaceConfigGetTotalBytesReceivedResponse = await FritzServiceOperationHandler.GetWanCommonInterfaceConfigGetTotalBytesReceivedAsync();
+#pragma warning disable S1481 // Unused local variables should be removed
             var theCopy = wanCommonInterfaceConfigGetTotalBytesReceivedResponse;
+#pragma warning restore S1481 // Unused local variables should be removed
         }
 
-
+        /*
         private async void AutoRefreshTimerTick(object? sender, EventArgs e)
         {
             if (CanExecuteDefaultCommand)
                 await DefaultCommand.ExecuteAsync(false);
-        }
-
-        /*
-        private async Task GetWanDslInterfaceConfigGetDSLDiagnoseInfoAsync()
-        {
-            WanDslInterfaceConfigGetDSLDiagnoseInfoResponse = await FritzServiceOperationHandler.WanDslInterfaceConfigGetDSLDiagnoseInfoAsync();
-        }
-
-        private async Task GetWanDslInterfaceConfigGetDSLInfoAsync()
-        {
-            WanDslInterfaceConfigGetDSLInfoResponse = await FritzServiceOperationHandler.WanDslInterfaceConfigGetDSLInfoAsync();
-        }
-
-        private async Task GetWanDslInterfaceConfigGetInfoAsync()
-        {
-            WanDslInterfaceConfigInfoControlViewModel.WanDslInterfaceConfigGetInfoResponse = await FritzServiceOperationHandler.WanDslInterfaceConfigGetInfoAsync();
-        }
-
-        private async Task GetWanDslInterfaceConfigGetStatisticsTotalAsync()
-        {
-            WanDslInterfaceConfigGetStatisticsTotalResponse = await FritzServiceOperationHandler.WanDslInterfaceConfigGetStatisticsTotalAsync();
         }
         */
     }
