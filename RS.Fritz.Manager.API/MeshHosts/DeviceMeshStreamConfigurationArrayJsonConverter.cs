534b36f22ecbdc8c3699e6762cc9d67b486c127c
Rans4ckeR <ransacker@outlook.com>
2022-03-20 23:36:33 +0100

    Add DeviceMeshStreamConfiguration custom JsonConverter

---
+++ RS.Fritz.Manager.API/MeshHosts/DeviceMeshStreamConfigurationArrayJsonConverter.cs
@@ -0,0 +1,58 @@
+﻿namespace RS.Fritz.Manager.API;
+
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+internal sealed class DeviceMeshStreamConfigurationArrayJsonConverter : JsonConverter<DeviceMeshStreamConfiguration[]>
+{
+    public override bool CanConvert(Type typeToConvert) =>
+        typeof(DeviceMeshStreamConfiguration[]).IsAssignableFrom(typeToConvert);
+
+    public override DeviceMeshStreamConfiguration[] Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType != JsonTokenType.StartArray)
+            throw new JsonException();
+
+        var deviceMeshStreamConfigurations = new Collection<DeviceMeshStreamConfiguration>();
+
+        while (reader.Read())
+        {
+            if (reader.TokenType == JsonTokenType.EndArray)
+                return deviceMeshStreamConfigurations.ToArray();
+
+            if (reader.TokenType != JsonTokenType.StartArray)
+                throw new JsonException();
+
+            _ = reader.Read();
+
+            if (reader.TokenType != JsonTokenType.String)
+                throw new JsonException();
+
+            string channelWidth = reader.GetString()!;
+
+            _ = reader.Read();
+
+            if (reader.TokenType != JsonTokenType.Number)
+                throw new JsonException();
+
+            int supportedStreamCount = reader.GetInt32();
+
+            _ = reader.Read();
+
+            if (reader.TokenType != JsonTokenType.EndArray)
+                throw new JsonException();
+
+            deviceMeshStreamConfigurations.Add(new DeviceMeshStreamConfiguration(channelWidth, supportedStreamCount));
+        }
+
+        throw new JsonException();
+    }
+
+    public override void Write(Utf8JsonWriter writer, DeviceMeshStreamConfiguration[] value, JsonSerializerOptions options)
+    {
+        throw new NotSupportedException();
+    }
+}
