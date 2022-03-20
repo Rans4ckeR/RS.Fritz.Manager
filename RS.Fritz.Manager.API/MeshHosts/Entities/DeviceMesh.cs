2a526a2933ab53c5b125c80fcc3a5f7b880bf27b
Rans4ckeR <ransacker@outlook.com>
2022-03-20 02:23:08 +0100

    DeviceMesh json parsing

--- RS.Fritz.Manager.API/MeshHosts/Entities/DeviceMesh.cs
+++ RS.Fritz.Manager.API/MeshHosts/Entities/DeviceMesh.cs
@@ -1,80 +1,66 @@
 ﻿namespace RS.Fritz.Manager.API;
 
+using System.Text.Json.Serialization;
+
 // https://avm.de/fileadmin/user_upload/Global/Service/Schnittstellen/mesh_topology/mesh_topology_schema_v1.9.json
-#pragma warning disable SA1516 // Elements should be separated by blank line
-#pragma warning disable CA1707 // Identifiers should not contain underscores
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-#pragma warning disable IDE1006 // Naming Styles
-#pragma warning disable SA1300 // Element should begin with upper-case letter
+public readonly record struct DeviceMesh(
+    [property: JsonPropertyName("schema_version")] string SchemaVersion,
+    [property: JsonPropertyName("nodes")] Node[] Nodes);
 
-public sealed record DeviceMesh
-{
-    public string schema_version { get; set; }
-    public Node[] nodes { get; set; }
-}
+public readonly record struct Node(
+    [property: JsonPropertyName("uid")] string Uid,
+    [property: JsonPropertyName("device_name")] string DeviceName,
+    [property: JsonPropertyName("device_model")] string DeviceModel,
+    [property: JsonPropertyName("device_manufacturer")] string DeviceManufacturer,
+    [property: JsonPropertyName("device_firmware_version")] string DeviceFirmwareVersion,
+    [property: JsonPropertyName("device_mac_address")] string DeviceMacAddress,
+    [property: JsonPropertyName("is_meshed")] bool IsMeshed,
+    [property: JsonPropertyName("mesh_role")] string MeshRole,
+    [property: JsonPropertyName("meshd_version")] string MeshdVersion,
+    [property: JsonPropertyName("node_interfaces")] NodeInterfaces[] NodeInterfaces);
 
-public sealed record Node
-{
-    public string uid { get; set; }
-    public string device_name { get; set; }
-    public string device_model { get; set; }
-    public string device_manufacturer { get; set; }
-    public string device_firmware_version { get; set; }
-    public string device_mac_address { get; set; }
-    public bool is_meshed { get; set; }
-    public string mesh_role { get; set; }
-    public string meshd_version { get; set; }
-    public NodeInterfaces[] node_interfaces { get; set; }
-}
+public readonly record struct NodeInterfaces(
+    [property: JsonPropertyName("uid")] string Uid,
+    [property: JsonPropertyName("name")] string Name,
+    [property: JsonPropertyName("type")] string Type,
+    [property: JsonPropertyName("mac_address")] string MacAddress,
+    [property: JsonPropertyName("blocking_state")] string BlockingState,
+    [property: JsonPropertyName("node_links")] NodeLinks[] NodeLinks,
+    [property: JsonPropertyName("ssid")] string Ssid,
+    [property: JsonPropertyName("opmode")] string Opmode,
+    [property: JsonPropertyName("security")] string Security,
+    [property: JsonPropertyName("supported_streams_tx")] object[][] SupportedStreamsTx,
+    [property: JsonPropertyName("supported_streams_rx")] object[][] SupportedStreamsRx,
+    [property: JsonPropertyName("current_channel")] int CurrentChannel,
+    [property: JsonPropertyName("phymodes")] string[] Phymodes,
+    [property: JsonPropertyName("channel_utilization")] int ChannelUtilization,
+    [property: JsonPropertyName("anpi")] int Anpi,
+    [property: JsonPropertyName("steering_enabled")] bool SteeringEnabled,
+    [property: JsonPropertyName("_11k_friendly")] bool Is11kFriendly,
+    [property: JsonPropertyName("_11v_friendly")] bool Is11vFriendly,
+    [property: JsonPropertyName("legacy_friendly")] bool LegacyFriendly,
+    [property: JsonPropertyName("rrm_compliant")] bool RrmCompliant,
+    [property: JsonPropertyName("channel_list")] ChannelList[] ChannelList);
 
-public sealed record NodeInterfaces
-{
-    public string uid { get; set; }
-    public string name { get; set; }
-    public string type { get; set; }
-    public string mac_address { get; set; }
-    public string blocking_state { get; set; }
-    public NodeLinks[] node_links { get; set; }
-    public string ssid { get; set; }
-    public string opmode { get; set; }
-    public string security { get; set; }
-    public object[][] supported_streams_tx { get; set; }
-    public object[][] supported_streams_rx { get; set; }
-    public int current_channel { get; set; }
-    public string[] phymodes { get; set; }
-    public int channel_utilization { get; set; }
-    public int anpi { get; set; }
-    public bool steering_enabled { get; set; }
-    public bool _11k_friendly { get; set; }
-    public bool _11v_friendly { get; set; }
-    public bool legacy_friendly { get; set; }
-    public bool rrm_compliant { get; set; }
-    public ChannelList[] channel_list { get; set; }
-}
+public readonly record struct NodeLinks(
+    [property: JsonPropertyName("uid")] string Uid,
+    [property: JsonPropertyName("type")] string Type,
+    [property: JsonPropertyName("state")] string State,
+    [property: JsonPropertyName("last_connected")] int LastConnected,
+    [property: JsonPropertyName("node_1_uid")] string Node1Uid,
+    [property: JsonPropertyName("node_2_uid")] string Node2Uid,
+    [property: JsonPropertyName("node_interface_1_uid")] string NodeInterface1Uid,
+    [property: JsonPropertyName("node_interface_2_uid")] string NodeInterface2Uid,
+    [property: JsonPropertyName("max_data_rate_rx")] int MaxDataRateRx,
+    [property: JsonPropertyName("max_data_rate_tx")] int MaxDataRateTx,
+    [property: JsonPropertyName("cur_data_rate_rx")] int CurDataRateRx,
+    [property: JsonPropertyName("cur_data_rate_tx")] int CurDataRateTx,
+    [property: JsonPropertyName("cur_availability_rx")] int CurAvailabilityRx,
+    [property: JsonPropertyName("cur_availability_tx")] int CurAvailabilityTx,
+    [property: JsonPropertyName("rx_rsni")] int RxRsni,
+    [property: JsonPropertyName("tx_rsni")] int TxRsni,
+    [property: JsonPropertyName("rx_rcpi")] int RxRcpi,
+    [property: JsonPropertyName("tx_rcpi")] int TxRcpi);
 
-public sealed record NodeLinks
-{
-    public string uid { get; set; }
-    public string type { get; set; }
-    public string state { get; set; }
-    public int last_connected { get; set; }
-    public string node_1_uid { get; set; }
-    public string node_2_uid { get; set; }
-    public string node_interface_1_uid { get; set; }
-    public string node_interface_2_uid { get; set; }
-    public int max_data_rate_rx { get; set; }
-    public int max_data_rate_tx { get; set; }
-    public int cur_data_rate_rx { get; set; }
-    public int cur_data_rate_tx { get; set; }
-    public int cur_availability_rx { get; set; }
-    public int cur_availability_tx { get; set; }
-    public int rx_rsni { get; set; }
-    public int tx_rsni { get; set; }
-    public int rx_rcpi { get; set; }
-    public int tx_rcpi { get; set; }
-}
-
-public sealed record ChannelList
-{
-    public int channel { get; set; }
-}
+public readonly record struct ChannelList(
+    [property: JsonPropertyName("channel")] int Channel);
