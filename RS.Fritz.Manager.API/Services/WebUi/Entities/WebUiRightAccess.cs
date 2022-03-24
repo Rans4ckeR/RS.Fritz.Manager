18323162813bbf717d45844354b899133f562340
Rans4ckeR <ransacker@outlook.com>
2022-03-24 22:38:48 +0100

    Add WebUiService

---
+++ RS.Fritz.Manager.API/Services/WebUi/Entities/WebUiRightAccess.cs
@@ -0,0 +1,11 @@
+﻿namespace RS.Fritz.Manager.API;
+
+using System.Xml.Serialization;
+
+public sealed record WebUiRightAccess : WebUiRight
+{
+    [XmlText]
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public string Access { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+}
