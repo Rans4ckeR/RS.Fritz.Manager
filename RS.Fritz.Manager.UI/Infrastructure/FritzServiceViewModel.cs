3d4aa7c7f5fd9a4bc7c99fe5a097252de47474ea
Rans4ckeR <ransacker@outlook.com>
2022-04-02 00:39:32 +0200

    Cleanup

--- RS.Fritz.Manager.UI/Infrastructure/FritzServiceViewModel.cs
+++ RS.Fritz.Manager.UI/Infrastructure/FritzServiceViewModel.cs
@@ -105,35 +105,23 @@ internal abstract class FritzServiceViewModel : ObservableRecipient, IRecipient<
     protected async Task<KeyValuePair<T?, UPnPFault?>> ExecuteApiAsync<T>(Func<InternetGatewayDevice, Task<T>> operation, IDictionary<ushort, string>? errorReasons = null, bool throwException = false)
         where T : struct
     {
-        UPnPFault error;
-
-        try
-        {
-            return new KeyValuePair<T?, UPnPFault?>(await operation(ApiDevice), null);
-        }
-        catch (FaultException<UPnPFault1> ex) when (!throwException)
-        {
-            error = new UPnPFault(ex.Detail.ErrorCode, ex.Detail.ErrorDescription);
-        }
-        catch (FaultException<UPnPFault2> ex) when (!throwException)
-        {
-            error = new UPnPFault(ex.Detail.ErrorCode, ex.Detail.ErrorDescription);
-        }
-
-        if (errorReasons?.TryGetValue(error.ErrorCode, out string? errorReason) ?? false)
-            error.ErrorReason = errorReason;
-
-        return new KeyValuePair<T?, UPnPFault?>(null, error);
+        return await DoExecuteApiAsync(operation(ApiDevice), errorReasons, throwException);
     }
 
     protected async Task<KeyValuePair<T?, UPnPFault?>> ExecuteApiAsync<T>(Func<InternetGatewayDevice, int, Task<T>> operation, int interfaceNumber, IDictionary<ushort, string>? errorReasons = null, bool throwException = false)
+        where T : struct
+    {
+        return await DoExecuteApiAsync(operation(ApiDevice, interfaceNumber), errorReasons, throwException);
+    }
+
+    private static async Task<KeyValuePair<T?, UPnPFault?>> DoExecuteApiAsync<T>(Task<T> operation, IDictionary<ushort, string>? errorReasons, bool throwException)
         where T : struct
     {
         UPnPFault error;
 
         try
         {
-            return new KeyValuePair<T?, UPnPFault?>(await operation(ApiDevice, interfaceNumber), null);
+            return new KeyValuePair<T?, UPnPFault?>(await operation, null);
         }
         catch (FaultException<UPnPFault1> ex) when (!throwException)
         {
